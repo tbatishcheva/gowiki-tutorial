93d0f5a1596c3dfd7b8e1b066c76ca6178b3bb3f
Tatiana Batishcheva <t.batishcheva@octonus.com>
2020-09-12 13:20:50 +0300

    Introducing the net/http package (an interlude)

--- wiki.go
+++ wiki.go
@@ -2,34 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
+	"net/http"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-
-	// the file should be created with read-write permissions for the current user only
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Page{Title: title, Body: body}, nil
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	p1 := &Page{Title: "Test page", Body: []byte("This is a simple page")}
-	p1.save()
-	p2, _ := loadPage("Test page")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
